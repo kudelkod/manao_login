27e90ed4082f8e273f7475810286be1966638cdf
kudelkod <kudelkod63@gmail.com>
2022-03-20 14:05:29 +0300

    Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'dmitriy/manao_login',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/bootstrap' => array(
+            'pretty_version' => '4.6.1',
+            'version' => '4.6.1.0',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/bootstrap',
+            'aliases' => array(),
+            'reference' => '97f06b5527fba4083b85b7bdb152513ad8c7d6ce',
+            'dev_requirement' => false,
+        ),
+        'components/jquery' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'reference' => '6cf38ee1fd04b6adf8e7dda161283aa35be818c3',
+            'dev_requirement' => false,
+        ),
+        'dmitriy/manao_login' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
